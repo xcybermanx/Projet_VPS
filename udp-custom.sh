b2e1175f019a07b462233d6f021305b813ee2a7a
xcybermanx <jawad@live.fr>
2025-11-05 00:09:01 +0100

    Create udp-custom.sh

---
+++ udp-custom.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd
+rm -rf /root/udp &>/dev/null
+mkdir -p /root/udp &>/dev/null
+wget https://raw.githubusercontent.com/FasterExE/UDP-Custom/main/udp-custom-linux-amd64 -O /root/udp/udp-custom &>/dev/null
+chmod +x /root/udp/udp-custom &>/dev/null
+wget https://raw.githubusercontent.com/FasterExE/UDP-Custom/main/config.json -O /root/udp/config.json &>/dev/null
+chmod 64 /root/udp/config.json &>/dev/null
+
+if [ -z "$1" ]; then
+cat <<EOF > /etc/systemd/system/custom.service
+[Unit]
+Description=UDP Custom by ePev. Team
+[Service]
+User=root
+Type=simple
+ExecStart=/root/udp/custom-server
+WorkingDirectory=/root/udp/
+Restart=always
+RestartSec=2
+[Install]
+WantedBy=default.target
+EOF
+else
+cat <<EOF > /etc/systemd/system/custom.service
+[Unit]
+Description=UDP Custom by ePev. Team
+[Service]
+User=root
+Type=simple
+ExecStart=/root/udp/custom-server -exclude $1
+WorkingDirectory=/root/udp/
+Restart=always
+RestartSec=2
+[Install]
+WantedBy=default.target
+EOF
+fi
+
+systemctl start custom &>/dev/null
+systemctl enable udp-custom &>/dev/null
